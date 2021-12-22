4c3571af2dc873832d008c21a7216b9100591e85
ZeldaSibuea22 <zelda.sibuea43@gmail.com>
2021-12-22 23:39:19 +0700

    add subject and teacher

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer-area section_gap">
+        <div class="container" align="center">
+
+    <h2 align="center" style="color:#fff"> SIM Sekolah</h2>
+    <span style="color:#fff" >&copy; eas_pweb2021_amanda_sabrina_zelda</a></span>
+        </div>
+    </footer>
